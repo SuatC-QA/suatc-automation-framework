7e880ad7da2ec64f57dad862664fd207b1ac8069
SuatC-QA <suatc.qa@gmail.com>
2025-11-27 19:39:20 -0800

    feat: add negative login scenarios with DataProvider

--- src/main/java/com/suatc/qa/pages/LoginPage.java
+++ src/main/java/com/suatc/qa/pages/LoginPage.java
@@ -5,9 +5,10 @@ import org.openqa.selenium.By;
 
 public class LoginPage extends BasePage {
 
-    private static final By USERNAME_FIELD = By.id("user-name");
-    private static final By PASSWORD_FIELD = By.id("password");
+    private static final By USERNAME_INPUT = By.id("user-name");
+    private static final By PASSWORD_INPUT = By.id("password");
     private static final By LOGIN_BUTTON = By.id("login-button");
+    private static final By ERROR_MESSAGE = By.cssSelector("[data-test='error']");
 
     public LoginPage open() {
         openBaseUrl();
@@ -27,11 +28,26 @@ public class LoginPage extends BasePage {
     }
 
     private void typePassword(String password) {
-        waitAndType(PASSWORD_FIELD, password);
+        waitAndType(PASSWORD_INPUT, password);
     }
 
     private void typeUsername(String username) {
-        waitAndType(USERNAME_FIELD, username);
+        waitAndType(USERNAME_INPUT, username);
+    }
+
+    /**
+     * Attempts login and stays on the LoginPage, used for negative/error scenarios.
+     */
+    public LoginPage attemptLogin(String username, String password) {
+        logger.info("Attempting login with username: {}", username);
+        waitAndType(USERNAME_INPUT, username);
+        waitAndType(PASSWORD_INPUT, password);
+        waitAndClick(LOGIN_BUTTON);
+        return this;
+    }
+
+    public String getErrorMessage() {
+        return waitForVisible(ERROR_MESSAGE).getText();
     }
 
 }
--- src/test/java/com/suatc/qa/tests/ui/LoginTest.java
+++ src/test/java/com/suatc/qa/tests/ui/LoginTest.java
@@ -4,6 +4,7 @@ import com.suatc.qa.base.BaseTest;
 import com.suatc.qa.config.ConfigReader;
 import com.suatc.qa.pages.InventoryPage;
 import com.suatc.qa.pages.LoginPage;
+import org.testng.annotations.DataProvider;
 import org.testng.annotations.Test;
 
 import static org.assertj.core.api.Assertions.assertThat;
@@ -11,7 +12,7 @@ import static org.assertj.core.api.Assertions.assertThat;
 public class LoginTest extends BaseTest {
 
     @Test
-    public void validUserCanLogin() {
+    public void shouldLoginSuccessfullyWithValidUser() {
         ConfigReader config = ConfigReader.getInstance();
         String username = config.getProperty("user.standard");
         String password = config.getProperty("pass.standard");
@@ -25,4 +26,37 @@ public class LoginTest extends BaseTest {
                 .as("Inventory page products title")
                 .isEqualTo("Products");
     }
+
+    @DataProvider(name = "invalidLoginData")
+    public Object[][] invalidLoginData() {
+        return new Object[][]{
+                // username, password, expectedError
+                {"standard_user", "wrong_password",
+                        "Epic sadface: Username and password do not match any user in this service"},
+                {"locked_out_user", "secret_sauce",
+                        "Epic sadface: Sorry, this user has been locked out."},
+                {"", "secret_sauce",
+                        "Epic sadface: Username is required"},
+                {"standard_user", "",
+                        "Epic sadface: Password is required"}
+        };
+    }
+
+    @Test(dataProvider = "invalidLoginData")
+    public void shouldShowErrorMessageForInvalidLogin(
+            String username,
+            String password,
+            String expectedError
+    ) {
+        LoginPage loginPage = new LoginPage()
+                .open()
+                .attemptLogin(username, password);
+
+        String actualError = loginPage.getErrorMessage();
+
+        assertThat(actualError)
+                .as("Error message for username='%s', password='%s'", username, password)
+                .isEqualTo(expectedError);
+    }
+
 }
