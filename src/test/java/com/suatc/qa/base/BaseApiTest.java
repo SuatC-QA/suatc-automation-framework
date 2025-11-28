77c757e7988ad744e9cb9b32ba4cb042a206254e
SuatC-QA <suatc.qa@gmail.com>
2025-11-27 21:45:50 -0800

    feat: add authenticated API tests using RestAssured

--- src/test/java/com/suatc/qa/base/BaseApiTest.java
+++ src/test/java/com/suatc/qa/base/BaseApiTest.java
@@ -1,13 +1,23 @@
 package com.suatc.qa.base;
 
 import com.suatc.qa.config.ConfigReader;
-import io.restassured.RestAssured;
+import io.restassured.builder.RequestSpecBuilder;
 import org.testng.annotations.BeforeClass;
+
+import static io.restassured.RestAssured.*;
 
 public abstract class BaseApiTest {
 
     @BeforeClass
     public void setUpApi() {
-        RestAssured.baseURI = ConfigReader.getInstance().getProperty("url.api");
+        ConfigReader config = ConfigReader.getInstance();
+
+        baseURI = config.getProperty("url.api");
+
+        String apiKey = config.getProperty("api.key");
+
+        requestSpecification = new RequestSpecBuilder()
+                .addHeader("x-api-key", apiKey)
+                .build();
     }
 }
--- src/test/java/com/suatc/qa/tests/api/UserApiTest.java
+++ src/test/java/com/suatc/qa/tests/api/UserApiTest.java
@@ -1,11 +1,12 @@
 package com.suatc.qa.tests.api;
 
 import com.suatc.qa.base.BaseApiTest;
+import io.restassured.http.ContentType;
 import org.testng.annotations.Test;
 
 import static io.restassured.RestAssured.given;
-import static org.hamcrest.Matchers.greaterThan;
-import static org.hamcrest.Matchers.notNullValue;
+import static org.hamcrest.Matchers.*;
+
 
 public class UserApiTest extends BaseApiTest {
 
@@ -20,4 +21,29 @@ public class UserApiTest extends BaseApiTest {
                 .body("data", notNullValue())
                 .body("data.size()", greaterThan(0));
     }
+
+    @Test
+    public void shouldCreateUser() {
+        given()
+                .contentType(ContentType.JSON)
+                .body("{\"name\":\"morpheus\",\"job\":\"leader\"}")
+                .when()
+                .post("/api/users")
+                .then()
+                .statusCode(201)
+                .body("name", equalTo("morpheus"))
+                .body("job", equalTo("leader"))
+                .body("id", notNullValue())
+                .body("createdAt", notNullValue());
+    }
+
+    @Test
+    public void shouldReturn404ForUnknownUser() {
+        given()
+                .when()
+                .get("/api/users/23")
+                .then()
+                .statusCode(404);
+    }
+
 }
